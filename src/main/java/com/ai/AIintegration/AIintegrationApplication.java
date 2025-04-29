49d34f2412afdf5ba5da3debf1e90a42d19685ce
kulklex <kulklex@gmail.com>
2025-04-29 12:08:10 +0100

    initial commit

---
+++ src/main/java/com/ai/AIintegration/AIintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.ai.AIintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIintegrationApplication {
+//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
+	public static void main(String[] args) {
+		SpringApplication.run(AIintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/AIintegration/AIintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.AIintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AIintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
